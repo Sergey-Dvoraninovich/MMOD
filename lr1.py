d6fd90d1acea6eb53fdc2f23762d266d4f7917b4
Sergey Dvoraninovich <sergey.dvoraninovich@gmail.ru>
2021-10-15 21:04:19 +0300

    math exp intervals

--- lr1.py
+++ lr1.py
@@ -1,5 +1,7 @@
 import numpy as np
 import math
+import scipy
+from scipy import stats
 import matplotlib.pyplot as plt
 from plots import histogram_3d, histogram
 
@@ -66,23 +68,66 @@ def get_data(matrix, n, m):
     mx = 0
     mx2 = 0
     for i in range(n):
-        mx += i * x[i]
-        mx2 += i**2 * x[i]
+        mx += (i+1) * x[i]
+        mx2 += (i+1)**2 * x[i]
     my = 0
     my2 = 0
-    for i in range(n):
-        my += i * y[i]
-        my2 += i**2 * y[i]
+    for i in range(m):
+        my += (i+1) * y[i]
+        my2 += (i+1)**2 * y[i]
     dx = mx2 - mx**2
     dy = my2 - my**2
     mxy = 0
     for i in range(n):
         for j in range(m):
-            mxy += i * j * matrix[i][j]
+            mxy += (i+1) * (j+1) * matrix[i][j]
     cov = mxy - mx * my
     rxy = cov / math.sqrt(dx * dy)
     return mx, my, dx, dy, cov, rxy
 
+def get_interval_data(values, n, m):
+    a = 0.05
+    values_amount = len(values)
+    y_values = []
+    x_values = []
+    for value in values:
+        y_values.append(value[1])
+        x_values.append(value[0])
+    x, y, sx, sy = 0, 0, 0, 0
+    for i in range(values_amount):
+        x += x_values[i]
+        y += y_values[i]
+    x /= values_amount
+    y /= values_amount
+    for i in range(values_amount):
+        sx += (x_values[i] - x) ** 2
+        sy += (y_values[i] - y) ** 2
+    sx /= values_amount - 1
+    sy /= values_amount - 1
+    x_delta = sx * stats.t.ppf((2 - a)/2, 10)
+    x_delta /= math.sqrt(values_amount - 1)
+    min_x = x - x_delta
+    max_x = x + x_delta
+    y_delta = sy * stats.t.ppf((2 - a)/2, 10)
+    y_delta /= math.sqrt(values_amount - 1)
+    min_y = y - y_delta
+    max_y = y + y_delta
+    return min_x, max_x, min_y, max_y
+
+
+
+def check_mizes(matrix, generated_matrix, n, m):
+    a = 0.05
+    critical_value = 0.461
+    result = 0
+    for i in range(n):
+        for j in range(m):
+            delta = matrix[i][j]
+            delta -= generated_matrix[i][j]
+            delta **= 2
+            result += delta
+    result = 1 / (12 * n * m) + result
+    return a, critical_value, result
 
 def make_probabilities_line(matrix):
     p_line = []
@@ -151,13 +196,26 @@ print("--- rxy ---- " + str(rxy))
 
 generated_matrix = normalize_matrix(values, values_amount, n, m)
 mx, my, dx, dy, cov, rxy = get_data(generated_matrix, n, m)
+min_x, max_x, min_y, max_y = get_interval_data(values, n, m)
 print("\n" + str(np.array(generated_matrix)))
 print("--- M[X] --- " + str(mx))
+print(str(min_x) + " - " + str(max_x))
 print("--- M[Y] --- " + str(my))
+print(str(min_y) + " - " + str(max_y))
 print("--- D[X] --- " + str(dx))
 print("--- D[Y] --- " + str(dy))
 print("--- cov ---- " + str(cov))
 print("--- rxy ---- " + str(rxy))
+
+
+print("\n------- Mizes criteria -------")
+a, critical_value, result = check_mizes(matrix, generated_matrix, n, m)
+print("Critical value - " + str(critical_value))
+print("        Result - " + str(result))
+#print(stats.chi2.isf(df=2, q=0.025))
+#print(stats.t.ppf((1 + 0.95)/2, 9))
+
+get_interval_data(values, n, m)
 
 
 
