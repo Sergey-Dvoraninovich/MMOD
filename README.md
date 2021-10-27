6cd17bfdde30411cfeda3bcb80a789735a739d06
Sergey Dvoraninovich <sergey.dvoraninovich@gmail.ru>
2021-10-27 22:21:15 +0300

    lr1 description

---
+++ README.md
@@ -0,0 +1,7 @@
+#LR1
+Work demonstration
+![LR1 Workstate 1](https://github.com/Sergey-Dvoraninovich/MMOD/sources/lr1_workstate1)
+![LR1 Workstate 1](https://github.com/Sergey-Dvoraninovich/MMOD/sources/lr1_workstate2)
+![LR1 plot 1](https://github.com/Sergey-Dvoraninovich/MMOD/sources/lr1_plot1)
+![LR1 plot 2](https://github.com/Sergey-Dvoraninovich/MMOD/sources/lr1_plot2)
+![LR1 plot 3](https://github.com/Sergey-Dvoraninovich/MMOD/sources/lr1_plot3)
--- lr2.py
+++ lr2.py
@@ -2,6 +2,7 @@ import random
 from enum import Enum
 import bisect
 import math
+import copy
 
 
 class ActionType(Enum):
@@ -26,6 +27,9 @@ class Request:
             result = True
         return result
 
+    def __eq__(self, other):
+        return self.id == other.id
+
     def __str__(self):
         return '({}, {}, {}, {}, {}, {})'.format(self.id, self.start_time, self.waiting_timeout, self.is_processed,
                                                  self.processing_time, self.waiting_time)
@@ -48,33 +52,46 @@ class Action:
 
 def calculate_theoretical_data(n, m, lambda_val, mu, v):
     alpha = lambda_val / mu
-    a = alpha / n
+    beta = v / mu
+
     p0 = 0
     for k in range(n + 1):
-        p0 += (alpha ** k) / math.factorial(k)
-    line_sum = a * (1 - (a**m))
-    line_sum /= (1 - a)
-    line_sum *= (alpha ** n) / math.factorial(n)
+        p0 += (lambda_val ** k) / (math.factorial(k) * (mu ** k))
+    line_sum = 0
+    for k in range(1, m + 1):
+        local_multiplication = 1
+        for j in range(1, k+1):
+            local_multiplication *= (n * mu + j * v)
+        line_sum += (lambda_val ** (n + k)) / (math.factorial(n) * (mu ** n) * local_multiplication)
     p0 += line_sum
     p0 = 1 / p0
 
     theoretical_p = [p0]
     for i in range(n):
         k = i + 1
-        pk = ((alpha ** k) / math.factorial(k)) * p0
+        pk = ((lambda_val ** k) / (math.factorial(k) * (mu ** k))) * p0
         theoretical_p.append(pk)
     for i in range(m):
         s = i + 1
-        pk = ((alpha ** (n + s)) / (math.factorial(n) * (n ** s))) * p0
+        local_multiplication = 1
+        for j in range(1, s+1):
+            local_multiplication *= (n * mu + j * v)
+        pk = ((lambda_val ** (n + s)) / (math.factorial(n) * (mu ** n) * local_multiplication)) * p0
         theoretical_p.append(pk)
 
-    decline_probability = ((alpha ** (n + m)) / ((n ** m) * math.factorial(n))) * p0
-    Q = 1 - decline_probability
-    A = lambda_val * Q
-    l_line = (alpha ** (n+1)) / (n * math.factorial(n)) * p0
-    l_line *= 1 - ((alpha/n)**m)*(m + 1 - m*alpha/n)
-    l_line /= ((1 - alpha/n) ** 2)
-    return decline_probability, l_line, theoretical_p
+    s = 0
+    for i in range(len(theoretical_p)):
+        s += i * theoretical_p[i]
+
+    p_decline = 1 - mu * s / lambda_val
+    #decline_probability = ((alpha ** (n + m)) / ((n ** m) * math.factorial(n))) * p0
+    # Q = 1 - decline_probability
+    # A = lambda_val * Q
+    #av_line_amount = (alpha ** (n+1)) / (n * math.factorial(n)) * p0
+    #av_line_amount *= 1 - ((alpha/n)**m)*(m + 1 - m*alpha/n)
+    #av_line_amount /= ((1 - alpha/n) ** 2)
+
+    return p_decline, None, theoretical_p
 
 
 def generate_requests(lambda_val, work_time, v):
@@ -89,7 +106,7 @@ def generate_requests(lambda_val, work_time, v):
         requests[i].id = i + 1
     return requests
 
-def get_empirical_p(state_log, print_result=False):
+def get_empirical_p(state_log, n, m):
     empirical_p = []
     for _ in range(n + m + 1):
         empirical_p.append(0)
@@ -105,12 +122,12 @@ def get_empirical_p(state_log, print_result=False):
         empirical_p[i] /= total_work_time
     return empirical_p
 
-def get_decline_probability(requests):
+def get_decline_probability(requests, n, m):
     declines_amount = 0
     for request in requests:
-         if not request.is_processed: # and request.waiting_time == 0:
+         if not request.is_processed and request.waiting_time == 0:
              declines_amount += 1
-    decline_probability = declines_amount / len(requests)
+    decline_probability = declines_amount / (len(requests))
     return decline_probability
 
 def get_requests_in_query(empirical_p):
@@ -128,12 +145,19 @@ def add_to_processing(request, actions, time=None):
     bisect.insort(actions, Action(request, processing_end_time, ActionType.FINISH_PROCESSING))
 
 
-n = 5
-m = 5
-lambda_val = 2
-mu = 0.5
-v = 0.5
-work_time = 9000
+# n = 5
+# m = 4
+# lambda_val = 2
+# mu = 0.5
+# v = 0.5
+# work_time = 180
+
+n = 1
+m = 1
+lambda_val = 1
+mu = 1
+v = 1
+work_time = 180
 
 requests = generate_requests(lambda_val, work_time, v)
 actions = []
@@ -184,23 +208,9 @@ while len(actions) != 0:
             state_log.append((processing_size, len(line), current_time))
 
     if current_action.action_type == ActionType.WAITING_TIMEOUT:
-        request.is_processed = False
         line.remove(request)
+        request.is_processed = False
         state_log.append((processing_size, len(line), current_time))
-
-# for event in state_log:
-#     print ("[" + str(event[0]) + " processing][" + str(event[1]) + " in line]["+str(event[2]) + "s]")
-#
-# for request in requests:
-#     line = ""
-#     if request.is_processed != True:
-#         line = "-DECLINED- "
-#     if request.is_processed is None:
-#         line = "-ERROR- "
-#     line += "[" + str(request.id) + "]"
-#     line += "[" + str(request.processing_time) + "s processed]"
-#     line += "[" + str(request.waiting_time) + "s in line]"
-#     print (line)
 
 
 decline_probability, line_requests_theoretical, theoretical_p = calculate_theoretical_data(n, m, lambda_val, mu, v)
@@ -212,12 +222,12 @@ print(decline_probability)
 print("Theoretical requests in query")
 print(line_requests_theoretical)
 
-empirical_p = get_empirical_p(state_log)
+empirical_p = get_empirical_p(state_log, n, m)
 print("Theoretical probabilities")
 print("   in processing: " + str(empirical_p[:n + 1]))
 print("         in line: " + str(empirical_p[n + 1:]))
 
-decline_probability = get_decline_probability(requests)
+decline_probability = get_decline_probability(requests, n, m)
 print("Empirical decline probability")
 print(decline_probability)
 line_requests_theoretical = get_requests_in_query(empirical_p)
